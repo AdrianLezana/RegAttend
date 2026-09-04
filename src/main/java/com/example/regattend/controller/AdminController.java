fdc2495a2d786081ae7c0a7d0b61a0f3542f6795
CristianVillaverde <cristianvillaverdec@gmail.com>
2026-09-04 10:07:03 -0400

    Se crearon los View mas un ejemplo de controller admin para que visualicen como funcionan

---
+++ src/main/java/com/example/regattend/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.example.regattend.controller;
+
+import javafx.fxml.FXML;
+
+public class AdminController {
+
+    @FXML
+    private void handleGenerarReporte() {
+        // Adrián, es un ejemplo para que veas como funcionan los handler :D estos controller manejan la lógica de los view
+        // Puedes agregar las funciones de salir, agregar, cambiar de página... etc. como en swing
+        System.out.println("Botón presionado: Generando reporte...");
+    }
+}
